61eeca4518f51610bbc4d118df6d16c29c3f566c
mrodriguez <mrodriguez@thepricehub.com>
2019-10-16 17:17:07 +0200

    define routes to add adds

--- app/src/Controller/HomeController.php
+++ app/src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\TwigBundle\TwigEngine;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class HomeController
@@ -11,7 +12,18 @@ class HomeController
     /** @var TwigEngine */
     public $templating;
 
+    /**
+     * @return Response
+     * @throws \Twig\Error\Error
+     */
     public function index() : Response{
         return new Response($this->templating->render('adds/adds.html.twig'));
+    }
+
+    /**
+     * @return Response
+     */
+    public function addAddAction(Request $request) {
+        return new Response();
     }
 }
